51842086332b9a078d3e7644d08ac763d0196f71
Nimai Behera <164841016+NimaiBehera1@users.noreply.github.com>
2024-03-25 10:51:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal");
+const openModalButton = document.querySelector(".openModalButton");
+const closeModalButton = document.querySelector(".close__modal");
+
+
+const openModal = () => {
+modal.classList.remove("close");
+}
+
+const closeModal = () => {
+    modal.classList.add("close");
+}
+
+openModalButton.addEventListener("click", () =>{
+    openModal();
+})
+
+closeModalButton.addEventListener("click", () =>{
+    closeModal();
+})
